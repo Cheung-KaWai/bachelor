6bb63a60e1d64fa3dfaa64725acdd0822af37dc0
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-24 16:09:07 +0200

    Add beam angle functionality

--- src/components/Configurator/Configurator.jsx
+++ src/components/Configurator/Configurator.jsx
@@ -3,6 +3,7 @@ import { LightContext } from "../../context/LightContextProvider";
 import { ConfigContainer } from "../Layout/ConfigContainer";
 import { GenerateRoom } from "./GenerateRoom";
 import { LightStep1 } from "./LightStep1";
+import { LightStep2 } from "./LightStep2";
 
 export const Configurator = () => {
   const lightContext = useContext(LightContext);
@@ -12,6 +13,8 @@ export const Configurator = () => {
       switch (lightContext.step) {
         case 1:
           return <LightStep1 />;
+        case 2:
+          return <LightStep2 />;
         default:
           return <GenerateRoom />;
       }
--- src/components/Configurator/LightStep2.jsx
+++ src/components/Configurator/LightStep2.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { LightContext } from "../../context/LightContextProvider";
+import { ConfigTitle } from "./ConfigTitle";
 
 export const LightStep2 = () => {
-  return <div>LightStep2</div>;
+  const lightContext = useContext(LightContext);
+
+  const handleClick = (ev) => {
+    const angle = ev.target.value;
+    lightContext.lightRef.current.angle = (angle * Math.PI) / 180;
+    console.log(lightContext.lightRef);
+  };
+
+  return (
+    <>
+      <ConfigTitle title={"Lamp Beam Angle"} />
+      <AngleContainer>
+        <button value={15} onClick={handleClick}>
+          15°
+        </button>
+        <button value={35} onClick={handleClick}>
+          35°
+        </button>
+        <button value={60} onClick={handleClick}>
+          60°
+        </button>
+      </AngleContainer>
+    </>
+  );
 };
+
+const AngleContainer = styled.div``;
