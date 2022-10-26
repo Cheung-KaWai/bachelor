d948e679a7fd9a07b64a8552108e7ef023af8b92
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-26 10:45:26 +0200

    Add test quickview

--- src/pages/QuikView.jsx
+++ src/pages/QuikView.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import image from "../assets/schoen.jpg";
+import usdz from "../assets/test.usdz";
 
 export const QuikView = () => {
   return (
     <div>
-      <a rel="ar" href="/assets/models/my-model.usdz">
-        <img src="/assets/models/my-model-thumbnail.jpg" />
+      <a rel="ar" href={usdz}>
+        <img src={image} />
       </a>
     </div>
   );
