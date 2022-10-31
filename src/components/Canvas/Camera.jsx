c3009b3c80c4d92d076df88d5dec99fe6f641264
Kawai cheung <kawaicheungdev@gmail.com>
2022-10-31 09:28:54 +0100

    delete files

--- src/components/Canvas/Camera.jsx
+++
@@ -1,6 +0,0 @@
-import { PerspectiveCamera, useHelper } from "@react-three/drei";
-import React from "react";
-
-export const Camera = () => {
-  return <PerspectiveCamera makeDefault position={[0, 20, 0]} />;
-};
