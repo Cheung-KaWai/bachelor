47df4eb0b027869ce26ae9c20c5f2dd6aa8ef237
aaron <kawaicheungdev@gmail.com>
2022-11-25 12:20:43 +0100

    Add scan functionality

--- roomscan/roomscan/Controllers/ScanController.swift
+++ roomscan/roomscan/Controllers/ScanController.swift
@@ -13,17 +13,32 @@ class ScanController: UIViewController, RoomCaptureViewDelegate, RoomCaptureSess
     private var roomCaptureView: RoomCaptureView!
     private var roomCaptureSessionConfig: RoomCaptureSession.Configuration = RoomCaptureSession.Configuration()
 
+    @IBOutlet weak var roomView: RoomCaptureView!
+    
+    @IBOutlet weak var done: UIImageView!
+    @IBOutlet weak var cancel: UIImageView!
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var exportButton: UIButton!
+    @IBOutlet weak var retakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setup()
-        print("ja wirim")
+        hideExportButtons()
+        setup()
     }
     
     private func setup() {
-        roomCaptureView = RoomCaptureView(frame: view.bounds)
-        roomCaptureView.captureSession.delegate = self
-        roomCaptureView.delegate = self
-        view.insertSubview(roomCaptureView, at: 0)
+        roomView.captureSession.delegate = self
+        roomView.delegate = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(doneClickked(tapGestureRecognizer:)))
+        done.isUserInteractionEnabled = true
+        done.addGestureRecognizer(tapGestureRecognizer)
+        
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(cancelClicked(tapGestureRecognizer:)))
+        cancel.isUserInteractionEnabled = true
+        cancel.addGestureRecognizer(tapGestureRecognizer2)
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -37,12 +52,63 @@ class ScanController: UIViewController, RoomCaptureViewDelegate, RoomCaptureSess
     
     private func startSession() {
         scanning = true
-        roomCaptureView?.captureSession.run(configuration: roomCaptureSessionConfig)
+        roomView?.captureSession.run(configuration: roomCaptureSessionConfig)
     }
     
     private func stopSession() {
         scanning = false
-        roomCaptureView?.captureSession.stop()
+        roomView?.captureSession.stop()
+    }
+    
+    @objc func doneClickked(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        stopSession()
+        hideButtons()
+        showExportButtons()
+        print("hello")
+    }
+    
+    @objc func cancelClicked(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        stopSession()
+        print("bye")
+        let vc = storyboard?.instantiateViewController(identifier: "tabBar") as! TabBarViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    private func hideExportButtons(){
+        exportButton.isHidden = true
+        retakeButton.isHidden = true
+        message.isHidden = true
+    }
+    
+    private func showExportButtons(){
+        exportButton.isHidden = false
+        retakeButton.isHidden = false
+        message.isHidden = false
+    }
+    
+    private func hideButtons(){
+        done.isHidden = true
+        cancel.isHidden = true
+    }
+    
+    private func showButtons(){
+        done.isHidden = false
+        cancel.isHidden = false
+    }
+    
+    
+    @IBAction func exportData(_ sender: Any) {
+    }
+    
+    @IBAction func retake(_ sender: Any) {
+        startSession()
+        showButtons()
+        hideExportButtons()
     }
 }
 
