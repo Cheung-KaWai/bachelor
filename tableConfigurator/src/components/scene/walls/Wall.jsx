64be79ef6e5577613323db29aa44b34005b03e4e
Kawai cheung <kawaicheungdev@gmail.com>
2022-12-15 16:45:40 +0100

    Add floorpoints

--- tableConfigurator/src/components/scene/walls/Wall.jsx
+++ tableConfigurator/src/components/scene/walls/Wall.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { useConfigurationStore } from "@/store/data";
+import React, { useEffect, useRef } from "react";
 import { Matrix4, Quaternion, Vector3 } from "three";
 
 export const Wall = ({ scale, transform }) => {
+  const wallRef = useRef();
+  const room = useConfigurationStore((state) => state.room);
+  const addFloorpoints = useConfigurationStore((state) => state.addFloorpoints);
+
   const { translation, rotation } = getTransformData(transform);
 
+  useEffect(() => {
+    generateCornerPoint(scale, wallRef, addFloorpoints);
+  }, [room]);
+
   return (
-    <mesh position={translation} quaternion={rotation}>
+    <mesh position={translation} quaternion={rotation} ref={wallRef}>
       <boxGeometry args={[scale[0], scale[1], 0.01]} />
       <meshStandardMaterial />
     </mesh>
@@ -25,4 +34,31 @@ const getTransformData = (transform) => {
     translation,
     rotation,
   };
+};
+
+const generateCornerPoint = (scale, wallRef, addFloorpoints) => {
+  const matrix = wallRef.current.matrixWorld;
+  console.log(matrix);
+  const vector1 = new Vector3(scale[0] / 2, -scale[1] / 2, 0);
+  const vector2 = new Vector3(-scale[0] / 2, -scale[1] / 2, 0);
+
+  vector1.applyMatrix4(matrix);
+  vector2.applyMatrix4(matrix);
+  vector1.set(vector1.x.toFixed(2), vector1.y.toFixed(2), vector1.z.toFixed(2));
+  vector2.set(vector2.x.toFixed(2), vector2.y.toFixed(2), vector2.z.toFixed(2));
+
+  const points = [];
+  const difX = parseFloat(vector2.x) - parseFloat(vector1.x);
+  const difZ = parseFloat(vector2.z) - parseFloat(vector1.z);
+
+  const pointsNumbers = 3;
+  const intervalX = difX / (pointsNumbers + 1);
+  const intervalZ = difZ / (pointsNumbers + 1);
+  for (let i = 1; i <= pointsNumbers; i++) {
+    points.push(
+      new Vector3(parseFloat(vector1.x) + intervalX * i, parseFloat(vector1.y), parseFloat(vector1.z) + intervalZ * i)
+    );
+  }
+
+  addFloorpoints([vector1, ...points, vector2]);
 };
--- tableConfigurator/src/store/data.js
+++ tableConfigurator/src/store/data.js
@@ -3,7 +3,9 @@ import { mountStoreDevtool } from "simple-zustand-devtools";
 
 export const useConfigurationStore = create((set) => ({
   room: {},
+  floorPoints: [],
   update: (name, value) => set({ [name]: value }),
+  addFloorpoints: (points) => set((state) => ({ floorPoints: [...state.floorPoints, ...points] })),
 }));
 
 // eslint-disable-next-line no-undef
