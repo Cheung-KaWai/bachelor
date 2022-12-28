281cde0ad4767b40e06e453dfbcc2cab79d1f39e
Cheung-KaWai <kawaicheungdev@gmail.com>
2022-12-28 11:35:42 +0100

    Mdofiy frame

--- tableConfigurator/src/components/scene/windows/Frame.jsx
+++ tableConfigurator/src/components/scene/windows/Frame.jsx
@@ -3,8 +3,12 @@ import React from "react";
 export const Frame = ({ translation, quaternion, scale, left }) => {
   return (
     <group position={translation} quaternion={quaternion}>
-      <mesh position={[0, scale[1] / 2, 0]}>
-        <boxGeometry args={[scale[0] + 0.02, 0.02, 0.04]} />
+      <mesh position={[0, scale[1] / 2 - 0.025, 0]}>
+        <boxGeometry args={[scale[0] + 0.02, 0.05, 0.04]} />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh position={[0, -scale[1] / 2 + 0.025, 0]}>
+        <boxGeometry args={[scale[0] + 0.02, 0.05, 0.04]} />
         <meshStandardMaterial />
       </mesh>
       <mesh position={[0, -scale[1] / 2, 0]}>
@@ -12,15 +16,15 @@ export const Frame = ({ translation, quaternion, scale, left }) => {
         <meshStandardMaterial />
       </mesh>
       <mesh position={[scale[0] / 2, 0, 0]}>
-        <boxGeometry args={[0.02, scale[1], 0.04]} />
+        <boxGeometry args={[0.05, scale[1], 0.04]} />
         <meshStandardMaterial />
       </mesh>
       <mesh position={[0, 0, 0]}>
-        <boxGeometry args={[0.02, scale[1], 0.04]} />
+        <boxGeometry args={[0.05, scale[1], 0.04]} />
         <meshStandardMaterial />
       </mesh>
       <mesh position={[-scale[0] / 2, 0, 0]}>
-        <boxGeometry args={[0.02, scale[1], 0.04]} />
+        <boxGeometry args={[0.05, scale[1], 0.04]} />
         <meshStandardMaterial />
       </mesh>
     </group>
